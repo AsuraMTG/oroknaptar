daa2e2a77244602235609ade6fe4e48c8676ade7
AsuraMTG <eper.karma@gmail.com>
2024-12-02 11:37:36 +0100

    V2

--- oroknaptar/Form1.Designer.cs
+++ oroknaptar/Form1.Designer.cs
@@ -33,6 +33,11 @@ namespace oroknaptar
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -40,14 +45,14 @@ namespace oroknaptar
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(241, 17);
+            this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Add meg a dátumot (YYYY-MM-DD): ";
+            this.label1.Text = "label1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Location = new System.Drawing.Point(12, 173);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(46, 17);
             this.label2.TabIndex = 1;
@@ -55,14 +60,14 @@ namespace oroknaptar
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(259, 9);
+            this.textBox1.Location = new System.Drawing.Point(255, 9);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(146, 22);
+            this.textBox1.Size = new System.Drawing.Size(156, 22);
             this.textBox1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(411, 8);
+            this.button1.Location = new System.Drawing.Point(255, 100);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
@@ -70,11 +75,58 @@ namespace oroknaptar
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(255, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(156, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(336, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(417, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(515, 9);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(156, 22);
+            this.textBox3.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -93,6 +145,11 @@ namespace oroknaptar
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- oroknaptar/Form1.cs
+++ oroknaptar/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,21 +20,51 @@ namespace oroknaptar
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            label1.Text = "Add meg a dátumot (YYYY-MM-DD): ";
             label2.Text = "";
+            label3.Text = "Esetleges esemény megadása:";
+            label4.Text = "Fájl neve:";
+
+            button1.Text = "Mutat";
+            button2.Text = "Törles";
         }
 
+        public string[] napok = { "Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat" };
+
         private void button1_Click(object sender, EventArgs e)
-        { 
+        {
+            if (label2.Text.Contains("A bevitt adat nem datum!"))
+                label2.Text = "";
+
             if (DateTime.TryParse(textBox1.Text, out DateTime datum))
             {
-                string[] napok = { "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap" };
-                string nap = napok[(int)datum.DayOfWeek - 1];
-                label2.Text = $"A megadott dátum: {datum.ToShortDateString()} {nap}i napra esett.";
+                string esemeny = "A megadott dátum";
+                string nap = napok[(int)datum.DayOfWeek];
+
+                if (textBox2.Text != "")
+                    esemeny = textBox2.Text;
+
+                string kalkulált = $"{esemeny}: {datum.ToShortDateString()} {nap}i napra esett.\n";
+                label2.Text += kalkulált;
+
+                if (textBox3.Text == "")
+                    textBox3.Text = "Datumok";
+
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine($"{textBox3.Text}.txt"), true))
+                    outputFile.Write($"{kalkulált}");
             }
             else
-            {
                 label2.Text = $"A bevitt adat nem datum!";
-            }
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine("Datumok.txt")))
+                outputFile.Write("");
         }
     }
 }
